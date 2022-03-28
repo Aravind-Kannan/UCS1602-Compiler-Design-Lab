6981e676bf92464637070103d0564094b580706d
aravind <aravindkannan2001@gmail.com>
2022-03-28 13:19:22 +0530

    Ex-01 Modified Input

--- Ex-01/sample.c
+++ Ex-01/sample.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
+// TODO: Compare two numbers
 main()
 {
     int a = 10, b = 20;
     char d = 'a';
     double e = 10.8;
     int x;
+    // d++;
     if (a > b)
         printf("a is greater");
     else
-        printf("b is greater"); // as
+        printf("b is greater"); 
+
 }
